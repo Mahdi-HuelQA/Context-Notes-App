2457f5f7c7c7bf47e2e856083477bf6f3fe1c5b1
Mahdi Haroun~ <mahdi@huel.com>
2022-05-18 09:39:49 +0100

    infinte loop of fretch fixed

--- src/Components/Quotes/Quotes.js
+++ src/Components/Quotes/Quotes.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './Quotes.css';
 
 const Quotes = () => {
   const [quote, setQuote] = useState([]);
-  const [num, setNum] = useState(2);
-
-  //fetch Quotes
-const calc = () => {
- return setNum( Math.floor(Math.random() * 100) + 1)
- }
  
-//  calc()
-//breaks app
+  //fetch Quotes
+  const calc = () => {
+    return Math.floor(Math.random() * 100) + 1;
+  };
+
+  //  calc()
+  //useeffect fixed infinite loop of re-rendering component
+
+  useEffect(() => {
+    fetchText();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   async function fetchText() {
-    
+    console.log('fetching');
     let response = await fetch('https://type.fit/api/quotes');
     let data = await response.json();
-    setQuote(data[num]);
+    setQuote(data[calc()]);
     console.log(data[3].text);
   }
-
-  fetchText();
 
   return (
     <div id='container'>
@@ -32,7 +35,6 @@ const calc = () => {
 
       <div class='photobanner'>
         <span class='first'>{quote.text} </span>
-      
       </div>
     </div>
   );
